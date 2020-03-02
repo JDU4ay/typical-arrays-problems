42682d240479a9e8e24299c79f1f4c3608760b59
JDU4ay <56123117+JDU4ay@users.noreply.github.com>
2020-03-02 22:08:29 +0500

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+    if (Array.isArray(array) === true && array.length >0) {
+        let minimal = array.sort(function (a, b) {
+            return a - b;
+        });
+        return minimal[0];
+    }
+    else {
+        return 0;
+    }
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+    if (Array.isArray(array) === true && array.length >0) {
+        let maximal = array.sort(function (a, b) {
+            return a - b;
+        });
+        return maximal[maximal.length - 1];
+    }
+    else{
+        return 0;
+    }
+};
 
 exports.avg = function avg (array) {
-  return 0;
+
+    if (Array.isArray(array) === true && array.length >0) {
+        let average = array.sort(function (a, b) {
+            return a - b;
+        });
+    let sum = 0;
+    for (let i = 0; i < average.length; i++) {
+        sum = sum + average[i];
+    }
+    return sum / average.length;
 }
+else {
+    return 0;
+    }
+};
